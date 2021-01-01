278afe480ae9c954828908a862b8a554b1fd46a7
kabariana <kabariana12@gmail.com>
2021-01-01 11:29:56 +0900

    Create 문제-10.c

---
+++ Answer DB/[도서]실전 C 프로그래밍/Chapter 9 _ [포인터]/문제-10.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void gcdlcm(int a, int b, int* _gcd, int* _lcm)
+{
+	int n = a > b ? b : a;
+	int gcd = 1, lcm;
+	for (int i = 1; i <= n; i++)
+		if (a % i == 0 && b % i == 0)
+			*_gcd = i;
+
+	*_lcm = (a * b) / (*_gcd);
+}
+
+int main()
+{
+	int a, b;
+	int gcd, lcm;
+	scanf("%d %d", &a, &b);
+
+	gcdlcm(a, b, &gcd, &lcm);
+
+	printf("%d %d", gcd, lcm);
+	return 0;
+}
