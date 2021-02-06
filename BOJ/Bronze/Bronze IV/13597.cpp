d0b14d56b9aa87545ed851d4f0e10ddac4a958d5
kabariana <kabariana12@gmail.com>
2021-02-06 18:45:22 +0900

    Update 13597.cpp

--- BOJ/Bronze/Bronze IV/13597.cpp
+++ BOJ/Bronze/Bronze IV/13597.cpp
@@ -5,7 +5,7 @@ int main(int argc, char* argv[])
     std::ios_base::sync_with_stdio(false);
     std::cin.tie(nullptr), std::cout.tie(nullptr);
 
-    int a, b;ㄸ
+    int a, b;
     std::cin >> a >> b;
 
     std::cout << std::max(a,b);
