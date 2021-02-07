f4c23f3d906616bd5c99e2b7e8a531632b2f973f
kabariana <kabariana12@gmail.com>
2021-02-07 21:01:21 +0900

    Create 11109.cpp

---
+++ BOJ/Bronze/Bronze III/11109.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int T, d, n, s, p;
+    std::cin >> T;
+    while(T--)
+    {
+        std::cin >> d >> n >> s >> p;
+        int st = n*s;
+        int pt = d + n*p;
+
+        if(pt < st)
+            std::cout << "paralleize\n";
+        else if(pt > st)
+            std::cout << "do not paralleize\n";
+        else
+            std::cout << "does not matter\n";
+    }
+    return 0;
+}
