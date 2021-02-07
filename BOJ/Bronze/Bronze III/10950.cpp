d80d2c70a75104811325114589fc17563f458acb
kabariana <kabariana12@gmail.com>
2021-02-07 21:18:30 +0900

    Create 10950.cpp

---
+++ BOJ/Bronze/Bronze III/10950.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int T, a, b;
+    std::cin >> T;
+    while(T--)
+    {
+        std::cin >> a >> b;
+        std::cout << a + b << '\n';
+    }
+    return 0;
+}
