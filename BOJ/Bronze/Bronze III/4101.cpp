70afa8044fa1893cca1cf437ad71c30748ed455e
kabariana <kabariana12@gmail.com>
2021-02-07 13:47:34 +0900

    Create 4101.cpp

---
+++ BOJ/Bronze/Bronze III/4101.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    while(true)
+    {
+        int a, b;
+        std::cin >> a >> b;
+        if(a == 0 && b == 0)
+            break;
+        else
+        {
+            if(a>b)
+                std::cout << "YES\n";
+            else
+                std::cout << "No\n";
+        }
+    }
+    return 0;
+}
