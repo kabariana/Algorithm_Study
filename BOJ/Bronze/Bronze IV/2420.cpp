9c54cd5fa9bcc5dcb8a2326d12ad1af1c29b0a58
kabariana <kabariana12@gmail.com>
2021-02-08 22:50:04 +0900

    Create 2420.cpp

---
+++ BOJ/Bronze/Bronze IV/2420.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+typedef long long int ll;
+
+int main(int argc, char* argv[])
+{
+    ll a, b, result;
+    std::cin >> a >> b;
+    if(a > b)
+        result = a - b;
+    else
+        result = b - a;
+}
