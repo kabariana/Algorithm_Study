8bb9762d2efe618102edff907ef6c6b25d693f70
kabariana <kabariana12@gmail.com>
2021-02-08 17:55:48 +0900

    Create 1330.cpp

---
+++ BOJ/Bronze/Bronze IV/1330.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int a, b;
+    std::cin >> a >> b;
+
+    if(a > b)
+        std::cout << ">";
+    else if(a < b)
+        std::cout << "<";
+    else
+        std::cout << "==";
+    return 0;
+}
