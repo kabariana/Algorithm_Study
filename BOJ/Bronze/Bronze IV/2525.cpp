7ffc2d6203f43301e4137b8557c3b3075339084e
kabariana <kabariana12@gmail.com>
2021-02-08 22:54:38 +0900

    Create 2525.cpp

---
+++ BOJ/Bronze/Bronze IV/2525.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int h, m, t;
+    std::cin >> h >> m >> t;
+
+    h += t/60;
+    m += t%60;
+
+    if(m >= 60)
+    {
+        h++;
+        m%=60;
+    }
+    if(h >= 24)
+        h %=24;
+    std::cout << h << ' ' << m;
+}
