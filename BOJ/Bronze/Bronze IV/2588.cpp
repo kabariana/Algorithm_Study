b76de9526336d5515cdd46c75ecb4ac26cb5fc6a
kabariana <kabariana12@gmail.com>
2021-02-09 13:51:24 +0900

    Create 2588.cpp

---
+++ BOJ/Bronze/Bronze IV/2588.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int a, b;
+    int r1, r2, r3, fin;
+    std::cin >> a >> b;
+
+    r1 = a*((b%100)%10);
+    r2 = a*((b%100)/10);
+    r3 = a*(b/100);
+    fin = a*b;
+
+    std::cout << r1 << '\n';
+    std::cout << r2 << '\n';
+    std::cout << r3 << '\n';
+    std::cout << fin;
+}
