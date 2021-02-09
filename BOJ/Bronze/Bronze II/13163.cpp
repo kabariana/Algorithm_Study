a3444ff0e928a1c2fb5d3fd0ae3032c445541a5d
kabariana <kabariana12@gmail.com>
2021-02-10 01:24:25 +0900

    commit

--- BOJ/Bronze/Bronze II/13163.cpp
+++ BOJ/Bronze/Bronze II/13163.cpp
@@ -5,25 +5,25 @@ int main(int argc, char* argv[])
     int T;
     std::cin >> T;
     std::cin.ignore();
-    while(T--)
+    while (T--)
     {
         std::string str;
         std::getline(std::cin, str);
         int index = 0;
-        for(int i = 0; str.size(); i++)
+        for (int i = 0; str[i]; i++)
         {
-            if(str[i] == ' ')
+            if (str[i] == ' ')
             {
                 index = i;
                 break;
             }
-            std::cout << "god";
-            for(int i = index + 1; str.size(); i++)
-            {
-                if(str[i] == ' ')
-                    continue;
-                std::cout << str[i];
-            }
+        }
+        std::cout << "god";
+        for (int i = index + 1; str[i]; i++)
+        {
+            if (str[i] == ' ')
+                continue;
+            std::cout << str[i];
         }
         std::cout << '\n';
     }
---
+++ BOJ/Bronze/Bronze III/14568.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int n, cnt = 0;
+    std::cin >> n;
+
+    for(int a = 1; a <= 100; a++)
+        for(int b = 1; b <= 100 - a; b++)
+            for(int c = 1; c <= 100 - a - b; c++)
+                if(a >= b + 2 && a%2 == 0 && b != 0 && c != 0 && a + b + c == n)
+                    cnt++;
+    std::cout << cnt <<'\n';
+    return 0;
+}
