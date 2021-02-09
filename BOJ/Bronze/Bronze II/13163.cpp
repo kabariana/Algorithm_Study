5bd43dfd0c393e9399a8d8fb6e76de8a51a4b8e9
kabariana <kabariana12@gmail.com>
2021-02-10 01:06:24 +0900

    Create 13163.cpp

---
+++ BOJ/Bronze/Bronze II/13163.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int T;
+    std::cin >> T;
+    std::cin.ignore();
+    while(T--)
+    {
+        std::string str;
+        std::getline(std::cin, str);
+        int index = 0;
+        for(int i = 0; str.size(); i++)
+        {
+            if(str[i] == ' ')
+            {
+                index = i;
+                break;
+            }
+            std::cout << "god";
+            for(int i = index + 1; str.size(); i++)
+            {
+                if(str[i] == ' ')
+                    continue;
+                std::cout << str[i];
+            }
+        }
+        std::cout << '\n';
+    }
+    return 0;
+}
