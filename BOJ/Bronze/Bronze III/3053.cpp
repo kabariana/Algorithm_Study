04ccdcb44a39a13a24fd47c482accec7789ee570
kabariana <kabariana12@gmail.com>
2021-02-09 17:41:18 +0900

    Create 3053.cpp

---
+++ BOJ/Bronze/Bronze III/3053.cpp
@@ -0,0 +1,12 @@
+#define _USE_MATH_DEFINES
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    double r;
+    std::cin >> r;
+
+    printf("%.6lf\n", M_PI*r*r);
+    printf("%.6lf", 2*r*r);
+    return 0;
+}
