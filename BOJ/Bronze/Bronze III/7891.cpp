ffc8ddc9c46f43d911b39596f42d519004fb2199
kabariana <kabariana12@gmail.com>
2021-02-10 00:22:44 +0900

    Create 7891.cpp

---
+++ BOJ/Bronze/Bronze III/7891.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int T;
+    std::cin >> T;
+    while(T--)
+    {
+        long long a, b, sum = 0;
+        std::cin >> a >> b;
+        sum = a + b;
+        std::cout << sum << '\n';
+    }
+    return 0;
+}
