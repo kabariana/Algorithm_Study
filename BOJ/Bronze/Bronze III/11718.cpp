1a83d11d00fbee3a73f695a4e74fd59ae626570b
kabariana <kabariana12@gmail.com>
2021-02-10 01:26:57 +0900

    Create 11718.cpp

---
+++ BOJ/Bronze/Bronze III/11718.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    while(true)
+    {
+        std::string str;
+        std::getline(std::cin, str);
+        if(str == "")
+            break;
+        std::cout << str << '\n';
+    }
+}
