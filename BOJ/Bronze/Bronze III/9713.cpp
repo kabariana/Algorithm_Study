b56208b611473e49b2955997d67f16bc9fd3f41e
kabariana <kabariana12@gmail.com>
2021-02-12 01:17:40 +0900

    Create 9713.cpp

---
+++ BOJ/Bronze/Bronze III/9713.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int T;
+    std::cin >> T;
+
+    while(T--)
+    {
+        int num, sum = 0;
+        std::cin >> num;
+
+        for(int i = 1; i <= num; i++)
+            if(i%2 == 1)
+                sum+=i;
+        std::cout << sum << '\n';
+    }
+    return 0;
+}
+
