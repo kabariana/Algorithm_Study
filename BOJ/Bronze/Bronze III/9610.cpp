d1d826c75bea9109dccb36d259a4fdc0c38191ea
kabariana <kabariana12@gmail.com>
2021-02-11 12:09:43 +0900

    Create 9610.cpp

---
+++ BOJ/Bronze/Bronze III/9610.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int n;
+    int arr[5] = {0, };
+    std::cin >> n;
+
+    while(n--)
+    {
+        int x, y;
+        std::cin >> x >> y;
+
+        if(x > 0 && y > 0)
+            arr[0]++;
+        else if(x < 0 && y < 0)
+            arr[1]++;
+        else if(x < 0 && y < 0)
+            arr[2]++;
+        else if(x > 0 && y < 0)
+            arr[3]++;
+        else
+            arr[4]++;
+    }
+    for(int i = 0; i<4; i++)
+        std::cout << "Q"<< i+1 <<  ": " << arr[i] <<'\n';
+    std::cout << "AXIS: " << arr[4];
+    return 0;
+}
