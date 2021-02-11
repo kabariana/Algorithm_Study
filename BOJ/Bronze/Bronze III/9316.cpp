45c95956b05e43fcb0332cefedc1b99c5d3f723a
kabariana <kabariana12@gmail.com>
2021-02-11 19:21:22 +0900

    Create 9316.cpp

---
+++ BOJ/Bronze/Bronze III/9316.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int n;
+    std::cin >> n;
+    for(int i = 1; i <=3; i++)
+        std::cout << "Hello World, Judge "<< i << "!\n";
+    return 0;
+}
