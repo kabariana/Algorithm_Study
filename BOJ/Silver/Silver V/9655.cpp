63ff9566edd3853507aa2f60ebb31347981dde5b
kabariana <kabariana12@gmail.com>
2021-02-11 19:22:02 +0900

    Create 9655.cpp

---
+++ BOJ/Silver/Silver V/9655.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int n;
+    std::cin >> n;
+
+    if(n%2 == 1)
+        std::cout << "SK";
+    else
+        std::cout << "CY";
+}
