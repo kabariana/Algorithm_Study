2e19f8c4f5412d11b9198e5fca8748bb904a3476
kabariana <kabariana12@gmail.com>
2021-02-11 19:28:11 +0900

    update

--- BOJ/Bronze/Bronze III/9316.cpp
+++ BOJ/Bronze/Bronze III/9316.cpp
@@ -4,7 +4,7 @@ int main(int argc, char* argv[])
 {
     int n;
     std::cin >> n;
-    for(int i = 1; i <=3; i++)
+    for(int i = 1; i <=n; i++)
         std::cout << "Hello World, Judge "<< i << "!\n";
     return 0;
 }
--- BOJ/Silver/Silver V/9655.cpp
+++ BOJ/Silver/Silver V/9655.cpp
@@ -9,4 +9,5 @@ int main(int argc, char* argv[])
         std::cout << "SK";
     else
         std::cout << "CY";
+    return 0;
 }
