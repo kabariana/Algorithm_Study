2d5901a912f5d277a596f4b54c97df678e9a35a8
kabariana <kabariana12@gmail.com>
2021-02-11 19:53:16 +0900

    Create 1834.cpp

---
+++ BOJ/Bronze/Bronze I/1834.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    long long n, sum = 0;
+    std::cin >> n;
+
+    for(long long i = 1; i < n; i++)
+    {
+        sum+=(n+1)*i;
+    }
+    std::cout << sum;
+    return 0;
+}
