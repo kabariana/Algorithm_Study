016e2db4a69212e380c1cbfd7914c9403b8d5a3f
kabariana <kabariana12@gmail.com>
2021-02-12 20:45:49 +0900

    Create 20332.cpp

---
+++ BOJ/Bronze/Bronze III/20332.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int n, sum = 0;
+    std::cin >> n;
+
+    for(int i = 0; i<n; i++)
+    {
+        int num;
+        std::cin >> num;
+        sum+=num;
+    }
+    if(sum%3==0)
+        std::cout << "yes";
+    else
+        std::cout << "no";
+}
