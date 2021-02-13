6c593952d973eab40f50714efdb2a1c606f780f2
kabariana <kabariana12@gmail.com>
2021-02-13 22:32:17 +0900

    Create 10996.cpp

---
+++ BOJ/Bronze/Bronze II/10996.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int n;
+    std::cin >> n;
+
+    for(int i = 1; i<=n*2; i++)
+    {
+        for(int j = 1; j<=n; j++)
+        {
+            if(i%2 == 0)
+            {
+                if(j%2==0)
+                    std::cout << "*";
+                else
+                    std::cout << " ";
+            }
+            else
+            {
+                if(j%2==0)
+                    std::cout << " ";
+                else
+                    std::cout << "*";
+            }
+        }
+        std::cout << '\n';
+    }    
+}
