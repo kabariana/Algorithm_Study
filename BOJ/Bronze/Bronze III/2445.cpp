9b1becd4c007f42a8367525628257027846f47b8
kabariana <kabariana12@gmail.com>
2021-02-13 20:00:21 +0900

    Create 2445.cpp

---
+++ BOJ/Bronze/Bronze III/2445.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int n;
+    std::cin >> n;
+
+    for(int i = 1; i<=n; i++)
+    {
+        for(int j = 0; j<i; j++)
+            std::cout << "*";
+        for(int j = 0; j<2*(n-i); j++)
+            std::cout << " ";
+        for(int j = 0; j<i; j++)
+            std::cout << "*";
+        std::cout << '\n';
+    }
+    for(int i = 1; i<n; i++)
+    {
+        for(int j = 0; j<n-i; j++)
+            std::cout << "*";
+        for(int j = 0; j<2*i; j++)
+            std::cout << " ";
+        for(int j = 0; j<n-i; j++)
+            std::cout << "*";
+        std::cout << '\n';
+    }
+    return 0;
+}
