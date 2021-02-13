07605c26c1af378d442d77874803e11459f87c5a
kabariana <kabariana12@gmail.com>
2021-02-13 22:26:00 +0900

    Update 10992.cpp

--- BOJ/Bronze/Bronze III/10992.cpp
+++ BOJ/Bronze/Bronze III/10992.cpp
@@ -16,7 +16,7 @@ int main(int argc, char* argv[])
         {
             std::cout << "*";
             for(int j = 1; j<=(i-1)*2-1; j++)
-                std::cout << "*";
+                std::cout << " ";
             std::cout << "*";
         }
         std::cout << '\n';
