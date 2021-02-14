8c0ad6ce7f6eaa442bdba53fe2c495f83ad4d6a2
kabariana <kabariana12@gmail.com>
2021-02-15 01:43:45 +0900

    Create 9094.cpp

---
+++ BOJ/1.Bronze/Bronze III/9094.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int t;
+    std::cin >> t;
+    while(t--)
+    {
+        int n, m;
+        std::cin >> n >> m;
+
+        int cnt = 0;
+        for(int a = 1; a<n; a++)
+        {
+            for(int b = 1; b<n; b++)
+            {
+                if((a*a+b*b+m)%(a*b) == 0 && b>a)
+                    cnt++;
+            }
+        }
+        std::cout << cnt <<'\n';
+    }
+    return 0;
+}
