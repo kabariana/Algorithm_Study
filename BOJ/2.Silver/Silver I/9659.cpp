7bd765a80c08c4b1996b26d207d33f5d8d74f9e7
kabariana <kabariana12@gmail.com>
2021-02-14 09:51:38 +0900

    Create 9659.cpp

---
+++ BOJ/2.Silver/Silver I/9659.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    long long n;
+    std::cin >> n;
+
+    if(n%2 == 1)
+        std::cout << "SK";
+    else
+        std::cout << "CY"
+}
