4f014cc755ee6514138c776ca06bfdbb9ac75b13
kabariana <kabariana12@gmail.com>
2021-02-14 23:13:51 +0900

    Create 8387.cpp

---
+++ BOJ/1.Bronze/Bronze II/8387.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int n;
+    std::string a, b;
+    std::cin >> n >> a >> b;
+
+    int cnt = 0;
+    for(int i = 0; i<n; i++)
+    {
+        if(a[i]==b[i])
+            cnt++;
+    }
+    std::cout << cnt;
+    return 0;
+}
