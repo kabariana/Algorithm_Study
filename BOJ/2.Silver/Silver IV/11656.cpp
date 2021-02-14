e656a33ff7d75f8d65c9820c2449955fe087bb65
kabariana <kabariana12@gmail.com>
2021-02-14 09:11:38 +0900

    Create 11656.cpp

---
+++ BOJ/2.Silver/Silver IV/11656.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    std::string str;
+    std::vector<std::string> v;
+
+    std::cin >> str;
+
+    for(int i = 0; str[i]; i++)
+    {
+        std::string tmp = str.substr(i, str.length()-1);
+        v.push_back(tmp);
+    }
+    std::sort(v.begin(), v.end());
+
+    for(int i = 0; i<v.size(); i++)
+        std::cout << v[i] << '\n';
+    return 0;
+}
