3fe83898e9242f4ca1db7954c8b8100d7f0af339
kabariana <kabariana12@gmail.com>
2021-02-16 23:09:52 +0900

    Create 20350.cpp

---
+++ BOJ/1.Bronze/Bronze II/20350.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    std::string str, tmp;
+    std::cin >> str;
+
+    tmp = str;
+    
+    for(int i = 0; str[i]; i++)
+        str[i] = tmp[(i+4)%str.size()];
+
+    tmp = "";
+    for(int i = 0; str[i]; i++)
+    {
+        if(std::isalpha(str[i]))
+            tmp+=std::to_string(str[i]-'A' + 10);
+        else
+            tmp+=str[i];
+    }
+    int sum = 0;
+    for(int i = tmp.size()-1, j = 1; i>=0; i--, j=(10*j)%97)
+    {
+        sum+=(tmp[i]-'0')*j;
+        sum%=97;
+    }
+    if(sum == 1)
+        std::cout << "correct";
+    else
+        std::cout << "incorrect";
+    return 0;
+}
