2325d782bf0e2046806a81fbc9c64e47d9f8afbc
kabariana <kabariana12@gmail.com>
2021-02-17 08:54:39 +0900

    Create 4299.cpp

---
+++ BOJ/1.Bronze/Bronze IV/4299.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int x, y;
+    bool flag = false;
+    std::cin >> x >> y;
+
+    for(int i = 0; i<=x; i++)
+    {
+        if(2*i - y == x)
+        {
+            std::cout << i << ' ' << i-y;
+            flag = true;
+            break;
+        }
+    }
+    if(!flag)
+}
