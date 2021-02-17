36a4500097c50375cac538dfff69dfc680b62f1e
kabariana <kabariana12@gmail.com>
2021-02-17 21:51:33 +0900

    Update 14581.cpp

--- BOJ/1.Bronze/Bronze II/14581.cpp
+++ BOJ/1.Bronze/Bronze II/14581.cpp
@@ -9,4 +9,6 @@ int main(int argc, char* argv[])
 
     std::cout << fan << fan << fan << '\n';
     std::cout << fan << ":" << str << ":" << fan << '\n';
+    std::cout << fan << fan << fan;
+    return 0;
 }
