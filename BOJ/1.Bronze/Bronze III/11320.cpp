4f25bb7b1f3bbe01e53d20bd6d059049f8581968
kabariana <kabariana12@gmail.com>
2021-02-18 01:17:42 +0900

    Create 11320.cpp

---
+++ BOJ/1.Bronze/Bronze III/11320.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+int getGCD(int a, int b)
+{
+    if(b == 0)
+        return a;
+    return getGCD(b, a%b);
+}
+
+int main(int argc, char* argv[])
+{
+    int t;
+    std::cin >> t;
+    while(t--)
+    {
+        int a, b;
+        std::cin >> a >> b;
+        int gcd = getGCD(a, b);
+        a /= gcd;
+        b /= gcd;
+        std::cout << a*a / b*b <<'\n';
+    }
+}
