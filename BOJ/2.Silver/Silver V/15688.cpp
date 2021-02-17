c383075384f468db3d6fadbb3bf0c4946885750b
kabariana <kabariana12@gmail.com>
2021-02-17 18:37:37 +0900

    Create 15688.cpp

---
+++ BOJ/2.Silver/Silver V/15688.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+    int n;
+    std::cin >> n;
+
+    int* arr = new int[n];
+
+    for(int i = 0; i<n; i++)
+        std::cin >> arr[i];
+
+    std::sort(arr, arr+n);
+
+    for(int i = 0; i<n; i++)
+        std::cout << arr[i] << '\n';
+
+    delete[] arr;
+}
