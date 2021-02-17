ed4c8ea649374a0712df6aa850e8aa46de1df015
kabariana <kabariana12@gmail.com>
2021-02-17 21:51:30 +0900

    Update 2455.cpp

--- BOJ/1.Bronze/Bronze III/2455.cpp
+++ BOJ/1.Bronze/Bronze III/2455.cpp
@@ -1,6 +1,22 @@
 #include <bits/stdc++.h>
 
+int arr[4];
+int on[4];
+int off[4];
+
 int main(int argc, char* argv[])
 {
-    
+    int maxv = 0;
+    for(int i = 0; i<4; i++)
+        std::cin >> on[i] >> off[i];
+
+    int temp = 0;
+    for(int i = 0; i<4; i++)
+    {
+        arr[i] = temp - on[i] + off[i];
+        temp = arr[i];
+        maxv = std::max(maxv, arr[i]);
+    }
+    std::cout << maxv;
+    return 0;
 }
