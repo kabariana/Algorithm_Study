c09a3ec459f62588c1f798bc1886ffad15ebc0b5
kabariana <kabariana12@gmail.com>
2021-02-17 23:28:27 +0900

    Create 1476.cpp

---
+++ BOJ/2.Silver/Silver V/1476.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int E, S, M, ans = 1;
+    int iE, iS, iM;
+    std::cin >> iE >> iS >> iM;
+
+    E = S = M = 1;
+
+    while((E!=iE) || (S!=iS) || (M!=iM))
+    {
+        if(E!=15)
+            E++;
+        else
+            E=1;
+        
+        if(S!=28)
+            S++;
+        else
+            S=1;
+        if(M!=19)
+            M++;
+        else
+            M=1;
+        ans++;
+    }
+    std::cout << ans;
+
+}
