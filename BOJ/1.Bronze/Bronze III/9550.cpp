2656c75d144735a052c2dedf68dbdebbf68b4ec4
kabariana <kabariana12@gmail.com>
2021-02-17 18:32:01 +0900

    Create 9550.cpp

---
+++ BOJ/1.Bronze/Bronze III/9550.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int T;
+    std::cin >> T;
+
+    while(T--)
+    {
+        int n, k;
+        std::cin >> n >> k;
+        
+        int cnt = 0;
+        for(int i = 0; i<n; i++)
+        {
+            int temp;
+            std::cin >> temp;
+            while(temp>=k)
+            {
+                cnt++;
+                temp-=k;
+            }
+        }
+        std::cout << cnt << '\n';
+    }
+}
