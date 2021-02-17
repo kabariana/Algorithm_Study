6c5322958863ed907655605e7ed9df3dbbd1a6ed
kabariana <kabariana12@gmail.com>
2021-02-17 21:41:50 +0900

    Create 14581.cpp

---
+++ BOJ/1.Bronze/Bronze II/14581.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    std::string str;
+    std::cin >> str;
+
+    std::string fan = ":fan:";
+
+    std::cout << fan << fan << fan << '\n';
+    std::cout << fan << ":" << str << ":" << fan << '\n';
+}
