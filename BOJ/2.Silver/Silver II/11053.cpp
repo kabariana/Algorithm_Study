f4cc41720c0938fd6a4bc5a4f6fe52aeb44b41b1
kabariana <kabariana12@gmail.com>
2021-02-18 12:51:36 +0900

    Create 11053.cpp

---
+++ BOJ/2.Silver/Silver II/11053.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+
+int dp[1001];
+
+int main(int argc, char* argv[])
+{
+    int n;
+    std::cin >> n;
+
+    int* arr = new int[n];
+    for(int i = 0; i<n; i++)
+        std::cin >> arr[i];
+
+    int ans = 1;
+    std::fill_n(dp, 1001, 1);
+    for(int i = 0; i<n; i++)
+    {
+        for(int j = 0; j<i; j++)
+        {
+            if(arr[i]>arr[j])
+                dp[i] = std::max(dp[i], dp[j]+1);
+        }
+        ans = std::max(ans, dp[i]);
+    }
+    std::cout << ans;
+    delete[] arr;
+    return 0;
+}
