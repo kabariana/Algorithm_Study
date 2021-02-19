b17582a66f36099fb17b81135824173f578d8ebe
kabariana <kabariana12@gmail.com>
2021-02-19 14:50:17 +0900

    Create 20361.cpp

---
+++ BOJ/1.Bronze/Bronze III/20361.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int n, x, k;
+    std::cin >> n >> x >> k;
+
+    for(int i = 0; i<k; i++)
+    {
+        int a, b;
+        std::cin >> a >> b;
+        if(a==x)
+            x=b;
+        else if(b==x)
+            x=a;
+    }
+    std::cout << x
+}
