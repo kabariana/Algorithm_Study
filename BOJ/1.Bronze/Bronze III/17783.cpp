d75fda69353d53ce768de3bb272a7d67933e3a68
kabariana <kabariana12@gmail.com>
2021-02-19 23:50:28 +0900

    Create 17783.cpp

---
+++ BOJ/1.Bronze/Bronze III/17783.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int n;
+    std::cin >> n;
+
+    if(n%2 == 0)
+        std::cout << "Alice\n1";
+    else
+        std::cout << "Bob";
+}
