d88c108cb81c41d550a9f967de958ea359bf4393
kabariana <kabariana12@gmail.com>
2021-02-19 15:40:43 +0900

    Create 19774.cpp

---
+++ BOJ/1.Bronze/Bronze III/19774.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+    int n;
+    std::cin >> n;
+
+    while(n--)
+    {
+        std::string str;
+        std::cin >> str;
+        int a = std::stoi(str.substr(0, 2));
+        int b = std::stoi(str.substr(2, 4));
+
+        if((a*a + b*b) % 7 == 1)
+            std::cout << "YES\n";
+        else
+            std::cout << "NO\n";
+    }
+}
