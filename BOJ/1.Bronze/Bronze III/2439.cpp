45d05a0e36a0b42632df07f43d65233f06052cc1
kabariana <kabariana12@gmail.com>
2021-02-20 20:06:15 +0900

    Update 2439.cpp

--- BOJ/1.Bronze/Bronze III/2439.cpp
+++ BOJ/1.Bronze/Bronze III/2439.cpp
@@ -10,5 +10,8 @@ int main(int argc, char* argv[])
         for(int j = 1; j <= n-i; j++)
             std::cout << " ";
         for(int j = 1; j<=i; j++)
+            std::cout << "*";
+        std::cout << '\n';
     }
+    return 0;
 }
