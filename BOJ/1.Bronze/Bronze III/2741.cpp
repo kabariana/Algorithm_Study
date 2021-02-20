eb6c6667af797a35edffc358bd59f06c24d748fd
kabariana <kabariana12@gmail.com>
2021-02-21 00:40:12 +0900

    Create 2741.cpp

---
+++ BOJ/1.Bronze/Bronze III/2741.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+    int n;
+    std::cin >> n;
+
+    for(int i = 1; i<=n; i++)
+        std::cout << i << '\n';
+    return 0;
+}
