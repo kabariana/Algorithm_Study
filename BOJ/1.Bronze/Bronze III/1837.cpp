661c4c68575f610af653783d80873f4c4e6c63a9
kabariana <kabariana12@gmail.com>
2021-02-20 15:24:35 +0900

    Create 1837.cpp

---
+++ BOJ/1.Bronze/Bronze III/1837.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+bool sieve[1000001];
+
+bool Prime(int num, std::string str)
+{
+    int sum = 0;
+    for(int i = 0; str[i]; i++)
+        sum = (sum*10 + (str[i]-'0'))%num;
+    if(sum == 0)
+        return true;
+    return false;
+}
+
+int main(int argc, char* argv[])
+{
+    int K;
+    std::string P;
+    std::cin >> P >> K;
+
+    bool flag = true;
+
+    int res=0;
+    for(int i=2; i<K; i++)
+    {
+        if(sieve[i])
+            continue;
+        if(Prime(i, P))
+        {
+            flag = false;
+            res = i;
+            break;
+        }
+        for(int j = 2*i; j < K; j+=i)
+            sieve[j] = true;
+    }
+    if(flag)
+        std::cout << "GOOD";
+    else
+        std::cout << "BAD " << res; 
+    return 0;
+}
