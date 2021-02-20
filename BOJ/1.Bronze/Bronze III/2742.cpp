d4a36ebcbc0b3fa0ea397ce2bc6ad6a269812150
kabariana <kabariana12@gmail.com>
2021-02-21 00:41:58 +0900

    Create 2742.cpp

---
+++ BOJ/1.Bronze/Bronze III/2742.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+    int n;
+    std::cin >> n;
+
+    for(int i = n; i>0; i++)   
+}
