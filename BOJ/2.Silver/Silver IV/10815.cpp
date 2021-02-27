979f654b77846c87cbc073cc3f49e1356e3c2e1c
kabariana <kabariana12@gmail.com>
2021-02-27 21:18:52 +0900

    Create 10815.cpp

---
+++ BOJ/2.Silver/Silver IV/10815.cpp
@@ -0,0 +1,46 @@
+// 이분탐색 문제
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+    int n;
+    std::cin >> n;
+    std::vector<int> v(n);
+    
+    for(int i = 0; i<n; i++)
+        std::cin >> v[i];
+    std::sort(v.begin(), v.end());
+    
+    int m;
+    std::cin >> m;
+
+    for(int i = 0; i<m; i++)
+    {
+        int num;
+        std::cin >> num;
+
+        int l = 0, h = n-1;
+        bool chk = false;
+        while(l<=h)
+        {
+            int mid = (l+h)/2;
+            if(v[mid] == num)
+            {
+                chk = true;
+                break;
+            }
+            else if(v[mid]<num)
+                l = mid + 1;
+            else
+                h = mid - 1;
+        }
+        if(chk)
+            std::cout << "1 ";
+        else
+            std::cout << "0 ";
+    }
+    return 0;
+}
