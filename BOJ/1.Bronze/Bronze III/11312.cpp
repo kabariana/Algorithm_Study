301d32a9e5b24ce101bde5e9144eb581df8b765b
kabariana <kabariana12@gmail.com>
2021-03-03 14:50:22 +0900

    Create 11312.cpp

---
+++ BOJ/1.Bronze/Bronze III/11312.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+    int T;
+    std::cin >> T;
+
+    while(T--)
+    {
+        int A, B;
+        std::cin >> A >> B;
+        std::cout << std::fixed << std::setprecision(0) << (A*A/2)/(B*B/2)<<'\n';
+    }
+    return 0;
+}
