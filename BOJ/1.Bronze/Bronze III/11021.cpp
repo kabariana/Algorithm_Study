0ddaabf76613fb9985550c266ac6eb6e0ed38a95
kabariana <kabariana12@gmail.com>
2021-03-03 14:41:34 +0900

    Create 11021.cpp

---
+++ BOJ/1.Bronze/Bronze III/11021.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+    int T;
+    std::cin >> T;
+
+    for(int i = 1; i<=T; i++)
+    {
+        int a, b;
+        std::cin >> a >> b;
+        std::cout << "Case #" << i << ": "<<a+b << '\n';
+    }
+    return 0;
+}
