59b9d5bdc3d59b35c334d015b094258c396b49ea
kabariana <kabariana12@gmail.com>
2021-03-04 23:48:10 +0900

    Create 14004.cpp

---
+++ BOJ/1.Bronze/Bronze III/14004.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+
+int main(int argc, char* argv[])
+{
+    ll a, b, c, d;
+    std::cin >> a >> b >> c >> d;
+    std::cout << (c+d)/(a-b);
+    return 0;
+}
