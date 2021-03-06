48ad177e01a6282454d95d2b3e3ef4e365639ce7
kabariana <kabariana12@gmail.com>
2021-03-06 23:04:19 +0900

    Create 16485.cpp

---
+++ BOJ/1.Bronze/Bronze III/16485.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int c, b;
+    std::cin >> c >> b;
+    std::cout << std::fixed << std::setprecision(6) << (double)c/b;
+    return 0;
+}
