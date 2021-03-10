1f035cfe7a1a7cfa03836da6d74c31203ec903a7
kabariana <kabariana12@gmail.com>
2021-03-10 21:07:42 +0900

    Create 8668.cpp

---
+++ BOJ/1.Bronze/Bronze III/8668.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+int main(int argc,char* argv[])
+{
+    int a, b;
+    std::cin >> a >> b;
+
+    if(2*b>a)
+        std::cout << "TAK";
+    else
+        std::cout << "NIE";
+    return 0;
+}
