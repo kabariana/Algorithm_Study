277494daf5d0e55fe17df37deb266a3ac722aa32
kabariana <kabariana12@gmail.com>
2021-05-15 23:13:57 +0900

    Create 21300.cpp

---
+++ BOJ/1.Bronze/Bronze V/21300.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int sum = 0;
+    for(int i = 0; i <6; i++)
+    {
+        int input;
+        cin >> input;
+        sum += input;
+
+    }
+    cout << sum * 5;
+    return 0; 
+}
