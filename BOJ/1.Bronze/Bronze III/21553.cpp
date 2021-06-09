e6a3b74b4c8db5ea682aea2ae0948a0620b1b90c
kabariana <kabariana12@gmail.com>
2021-06-09 14:19:30 +0900

    Create 21553.cpp

---
+++ BOJ/1.Bronze/Bronze III/21553.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+    std::string a, b;
+    std::cin >> a >> b;
+    std::cout << b;
+    return 0;
+}
