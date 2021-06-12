86316a756a4f2417f4003777a405ed221abe130d
kabariana <kabariana12@gmail.com>
2021-06-12 23:46:52 +0900

    Create 21756.cpp

---
+++ BOJ/1.Bronze/Bronze II/21756.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+    int n;
+    std::cin >> n;
+    std::cout << std::pow(2, int(std::log2(n)));
+    return 0;
+}
