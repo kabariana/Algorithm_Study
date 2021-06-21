f7783ce6d360081d00964bec4d1da3c3da22e8c9
kabariana <kabariana12@gmail.com>
2021-06-21 17:02:17 +0900

    Create 16953.cpp

---
+++ BOJ/2.Silver/Silver I/16953.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+    int a, b;
+    std::cin >> a >> b;
+
+    int cnt = 0;
+    while(true)
+    {
+        if(a > b)
+        {
+            std::cout << -1 << '\n';
+            break;
+        }
+
+        if(a == b)
+        {
+            std::cout << cnt + 1 << '\n';
+            break;
+        }
+
+        if( b % 10 == 1)
+            b/=10;
+        else if(b%2 == 0)
+            b/=2;
+        else
+        {
+            std::cout << -1 << '\n';
+            break;
+        }
+        cnt++;
+    }
+    return 0;
+}
