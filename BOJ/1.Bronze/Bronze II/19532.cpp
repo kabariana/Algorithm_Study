488d016a6381f07297f7d966bed87f9798552c45
kabariana <kabariana12@gmail.com>
2021-06-22 01:02:49 +0900

    Create 19532.cpp

---
+++ BOJ/1.Bronze/Bronze II/19532.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    int a, b, c, d, e, f;
+    std::cin >> a >> b >> c >> d >> e >> f;
+    int determinent = a*e - b*d;
+    std::cout << (e*c - b*f)/determinent << ' ' << (a*f-c*d)/determinent <<'\n';
+    return 0;
+
+}
