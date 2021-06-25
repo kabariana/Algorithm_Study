ac973590139850873cb1f54710cc95342eb2bea5
kabariana <kabariana12@gmail.com>
2021-06-26 00:26:20 +0900

    Create 1043.cpp

---
+++ BOJ/3.Gold/Gold IV/1043.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+#define MAX 50
+
+int N, M, K;
+int parents[MAX+1];
+std::vector<int> reveal;
+std::vector<std::vector<int>> v(MAX);
+
+int Find(int x)
+{
+    if(parents[x] == x)
+        return x;
+    return x = Find(parents[x]);
+}
+
+void _union(int x, int y)
+{
+    x = Find(x);
+    y = Find(y);
+    parents[x] = y;
+}
+
+
+int main(int argc, char* argv[])
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+    std::cin >> N >> M >> K;
+
+    while(K--)
+    {
+        int tmp;
+        std::cin >> tmp;
+        reveal.push_back(tmp);
+    }
+
+    for(int i = 1; i<=N; i++)
+        parents[i] = i;
+
+    for(int i = 0; i<M; i++)
+    {
+        int num;
+        std::cin >> num;
+        int x, y;
+        for(int j = 0; j<num; j++)
+        {
+            if(j >= 1)
+            {
+                y = x;
+                std::cin >> x;
+                _union(y, x);
+            }
+            else
+                std::cin >> x;
+            v[i].push_back(x);
+        }
+    }
+
+    for(auto& i : v)
+    {
+        bool flag = false;
+        for(auto& j : i)
+        {
+            if(flag) 
+                break;
+            for(auto& t : reveal)
+            {
+                if(Find(j) == Find(t))
+                {
+                    flag = true;
+                    break;
+                }
+            }
+        }
+        if(flag)
+            M--;
+    }
+    std::cout << M <<'\n';
+    return 0;
+}
