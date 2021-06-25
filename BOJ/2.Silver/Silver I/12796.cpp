2ee91d083179fa726001c995665f75fd34d649a4
kabariana <kabariana12@gmail.com>
2021-06-25 12:14:09 +0900

    Create 12796.cpp

---
+++ BOJ/2.Silver/Silver I/12796.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+    int k;
+    std::cin >> k;
+    std::cout << "3" <<'\n';
+    std::cout << "1 1 1 " << k + 1 << '\n';
+    return 0;
+}
