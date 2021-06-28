da1eaf8d157ba683b15a55cf6db4a4d3183e7692
kabariana <kabariana12@gmail.com>
2021-06-28 16:12:51 +0900

    Create 5386.cpp

---
+++ BOJ/4.Platinum/Platinum IV/5386.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+    int TC, s, k;
+    std::cin >> TC;
+
+    while(TC--)
+    {
+        std::cin >> s >> k;
+        if(k & 1)
+            std::cout << (s & 1) << '\n';
+        else
+        {
+            s %= k + 1;
+            std::cout << (s/k*k + s %2) <<'\n';
+        }
+    }
+    return 0;
+}
