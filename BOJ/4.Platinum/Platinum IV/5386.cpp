a3f3f74bc378f880c234000e342cb2b7c909a30e
kabariana <kabariana12@gmail.com>
2021-07-03 00:26:29 +0900

    Update 5386.cpp

--- BOJ/4.Platinum/Platinum IV/5386.cpp
+++ BOJ/4.Platinum/Platinum IV/5386.cpp
@@ -1,3 +1,9 @@
+// k가 홀수면 s의 홀짝에 따라 승패가 갈림
+// k가 짝수면 스프라그 - 그런디
+// k = 2 -> 0, 1, 2, 0, 1, 2
+// k = 4 -> 0, 1, 0, 1, 2, 0, 1, 0, 1, 2
+// k = 6 -> 0, 1, 0, 1, 0, 1, 2, 0, 1, 0, 1, 0, 1, 2
+
 #include <bits/stdc++.h>
 
 int main(int argc, char* argv[])
@@ -12,11 +18,14 @@ int main(int argc, char* argv[])
     {
         std::cin >> s >> k;
         if (k & 1) // odd
-            std::cout << (s % 2) << '\n';
+            std::cout << (s & 1) << '\n';
         else
         {
-            s %= k + 1;
-            std::cout << (s / k * k + s % 2) << '\n';
+            s %= (k + 1);
+            if(s == k)
+                std::cout << k << '\n';
+            else
+                std::cout << (s & 1) << '\n';
         }
     }
     return 0;
