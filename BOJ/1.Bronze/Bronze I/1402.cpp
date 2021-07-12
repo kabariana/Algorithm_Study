3932c24a1644d88a43819b09c955fb6d54abf52a
kabariana <kabariana12@gmail.com>
2021-07-13 01:21:24 +0900

    Create 1402.cpp

---
+++ BOJ/1.Bronze/Bronze I/1402.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+int main(int argc, char* argv[])
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+    int T, A, B;
+    std::cin >> T;
+
+    while(T--){
+        std::cout << "yes\n";
+    }
+    return 0;
+}
